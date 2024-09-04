f28af38aeafae73ca59edac6eab83c900fcc6031
theminhdayma <147359915+theminhdayma@users.noreply.github.com>
2024-09-04 13:47:06 +0700

    Add deploy API

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
-const jsonServer = require("json-server")
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
-// Tạo máy chủ
-const server = jsonServer.create()
+server.use(middlewares);
 
-const router = jsonServer.router("db.json")
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+    "/users/:resource/:id/show": "/:resource/:id",
+  })
+);
+server.use(router);
 
-const middlewares = jsonServer.defaults()
-
-server.use(middlewares)
-server.use(router)
-
-// Lắng nghe cổng của ứng dụng 
 server.listen(3000, () => {
-    console.log("http://localhost:3000");
-})
+  console.log("JSON Server is running");
+});
 
-module.exports = server
+// Export the Server API
+module.exports = server;
